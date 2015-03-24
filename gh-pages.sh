d6091caf6564be14605564ef24886c6116e744c2
azazdeaz <azazdeaz@gmail.com>
2015-03-24 09:51:50 +0100

    edit gh-pages.sh

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,2 +1,17 @@
-git checkout -b temp \
-rm build/.gitignore
+git checkout -B temp && \
+
+cd demo/ && \
+npm run build && \
+cd .. && \
+
+rm -f demo/.gitignore && \
+git add demo/build && \
+git commit -m 'deploy to gh-pages' && \
+git push origin :gh-pages --force
+git subtree push --prefix demo/build origin gh-pages
+#git push origin `git subtree split --prefix build master`:gh-pages -f && \
+
+git checkout master && \
+git branch -D temp && \
+
+echo done 0;
