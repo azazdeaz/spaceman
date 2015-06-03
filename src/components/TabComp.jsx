6f74904c0d9629dc45cb92732ff5f7c6fc5f9731
Polgár András <azazdeaz@gmail.com>
2015-06-03 14:55:28 +0200

    fix

--- src/components/TabComp.jsx
+++ src/components/TabComp.jsx
@@ -13,7 +13,7 @@ export default class TabComp extends React.Component {
     var {icon, label, content, action} = this.props;
 
     return <div
-      style={{width: '100%', height: '100%'}}
+      style={{display: 'flex', flex: 1}}
       openable={!!content}
       onSelect={() => action && action()}
       icon={icon}
